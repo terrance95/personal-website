8b7fb25eeac2984a782ce312905db84a9e1f036b
Terrance Reynolds <terrancereynolds@protonmail.com>
2021-04-19 16:05:52 +0900

    comment out code

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Box, Text } from 'theme-ui';
-import SEO from '../components/seo';
+// import SEO from '../components/seo';
 
 const NotFoundPage = () => (
 	<Box>
-		<SEO title="404: Not found" />
+		{/* <SEO title="404: Not found" /> */}
 		<Text>Page Not Found</Text>
 		<p>You just hit a route that doesn&#39;t exist... the sadness.</p>
 	</Box>
