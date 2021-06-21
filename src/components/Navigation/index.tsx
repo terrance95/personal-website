e03b2378422d21f69afd2f94a47eb918bda4c044
Terrance Reynolds <terrancereynolds@protonmail.com>
2021-06-21 22:28:12 +0900

    fix blur

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -50,15 +50,14 @@ const Navigation: FC<{ setMenu: () => void }> = ({ setMenu }) => {
 				<Box
 					sx={{
 						bg: 'primary',
-						width: ['16rem', '16rem'],
-						height: ['16rem', '16rem'],
+						width: ['16rem', '18rem'],
+						height: ['16rem', '18rem'],
 						top: '50%',
 						left: '50%',
 						position: 'absolute',
 						transform: 'translate(-50%, -50%)',
 						opacity: [0.1, 0.05],
-						filter: 'blur(100rem)',
-						//borderRadius: '50%',
+						filter: 'blur(4rem)',
 						zIndex: 0,
 					}}
 				/>{' '}
